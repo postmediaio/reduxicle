7b60cf8b63e31c394b6b4a8e7aef82e18dbee39d
Ben <benlorantfy@gmail.com>
2018-06-29 12:36:52 -0400

    remove unused script

--- .circleci/test.sh
+++
@@ -1,6 +0,0 @@
-#! bash
-reduxicle_core_version="23"
-
-                        if [[ -n $reduxicle_core_version && $reduxicle_core_version != "null" ]]; then
-                            cat package.json | jq '.dependencies["@reduxicle/core"] = "rc"'
-                        fi
